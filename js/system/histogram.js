561e7411067ca63672115e4e8d042f8e6c1b1690
Platane <brongniart.arthur@gmail.com>
2015-02-13 17:17:02 +0100

    fix histogram

--- js/system/histogram.js
+++ js/system/histogram.js
@@ -1,14 +1,23 @@
-var discretise = function( keys, w, offset ){
+var discretise = function( keys, w, _offset ){
 
     w = w || 1
-    offset = offset || 0
+    _offset = _offset || 0
 
+    // compute min
+    var offset = -_offset
+    for( var k in keys )
+        if( keys[ k ].length )
+            offset = Math.max( offset, -Math.floor( keys[ k ][ 0 ].date/ w ) )
+    offset *= -w
+
+    // count key number in each intervalle
     var q=[]
     for( var k in keys )
         for( var i = keys[ k ].length; i--; ){
-            var x = 0 | ( ( keys[ k ][ i ].date - offset ) / w )
+            var x = Math.floor( ( keys[ k ][ i ].date - offset ) / w )
             q[ x ] = ( 0 | q[ x ])  + 1
         }
+
 
     return {
         offset: offset,
